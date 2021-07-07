36efdf7d7a2ad5abfcc5b8dd8df1cec0970d9e0f
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-07-07 17:37:30 -0400

    Put user is functional

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -30,10 +30,15 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+    .then(newUser => {
+      return User.getById(req.params.id)
+    })
+    .then(user => {
+      res.json(user)
+    })
+    .catch(next)
 });
 
 router.delete('/:id', (req, res) => {
