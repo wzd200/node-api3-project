8c5b4365dfec4e94b10b1e9ebcd1b084fe8ae2be
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-07-07 17:00:28 -0400

    Set up initial middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,54 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`${req.method} request`)
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const { id } = req.params
+    console.log(`the id is ${id}`)
+    const user = await User.getById(id)
+    if (user) {
+      req.hub = user
+      next()
+    } else {
+      next({
+        status: 404,
+        message: `user not found`,
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.name) {
+    next({
+      status: 400,
+      message: 'missing required name field'
+    })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.text) {
+    next({
+      status: 400,
+      message: 'missing required text field'
+    })
+  } else {
+    next()
+  }
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
